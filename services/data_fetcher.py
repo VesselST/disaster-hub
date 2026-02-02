c62c0101d2c0122ba4f672e0e02bd9d9e6521d6e
Veesl <ianliu817@gmail.com>
2026-02-02 17:16:08 +0800

    feat:data_fetch.py 改為自動搜索宜花東json檔 把json文字轉為shelter物件

--- services/data_fetcher.py
+++ services/data_fetcher.py
@@ -5,32 +5,31 @@ from models.shelter import Shelter
 #指定資料路徑
 class DataFetcher:
     def __init__(self):
-        self.data_path = "data.json"
+        self.data_path = [
+            "data_for_refuge\\yilan_shelters.json",
+            "data_for_refuge\\hualian_shelters.json",
+            "data_for_refuge\\taitung_shelters.json"
+        ]
 
     #將本地json資料轉為sheltr物件清單
     def get_shelters(self) -> list[Shelter]:
-        if not os.path.exists(self.data_path):
-            print(f"error:cannot find the dataflie {self.data_path}")
-            return []
-
         try:
-            with open(self.data_path, 'r', encoding='utf-8') as f:
-                raw_data = json.load(f)
-                
-            shelters = []
-            #將所有json所有格式轉為shelter中定義好的模型
-            for item in raw_data:
-                shelter = Shelter(
-                    name=item.get("name"),
-                    total_vessel=item.get("Total_vessel", 0),
-                    total_people=item.get("Total_people", 0),
-                    lat=item.get("lat", 0.0),
-                    lon=item.get("lon", 0.0)
-                )
-                shelters.append(shelter)
-            
-            return shelters
+            with open('data/data_for_refuge.json', 'r', encoding='utf-8') as f:
+                data = json.load(f)
         
+            all_shelters = []
+            #遍歷宜花東key
+            for region in ['yilan_shelter', 'hualian_shelter', 'taitung_shelter']:
+                region_data = data.get(region, [])
+                for item in region_data:
+                        all_shelters.append(Shelter(
+                        name=f"[{region[:2]}] {item['name']}", # 加上地區前綴方便識別
+                        capacity=item['capacity'],
+                        current_ppl=item.get('current_ppl', 0),
+                        lat=item['lat'],
+                        lon=item['lon']
+                    ))
+            return all_shelters
         except Exception as e:
-            print(f"catching json reading error: {e}")
-            return []
+            print(f"fetch data error: {e}")
+        return []
